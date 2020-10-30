7530181b1e18494e1e198c46562dc3e5fa47fad9
unknown <metybuza46@gmail.com>
2020-10-30 13:04:08 -0600

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,7 +17,7 @@ const  urlsToCache = [
   self.addEventListener("install", (event) => {
     event.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
+        return cache.addAll(urlsToCache);
       })
     );
   
