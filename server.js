5a6e9ff6d806a066947587b320ceb6833b995b42
unknown <metybuza46@gmail.com>
2020-10-30 17:38:22 -0600

    updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://budget:enguMY@2019@ds051585.mlab.com:51585/heroku_7brj2whl", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
